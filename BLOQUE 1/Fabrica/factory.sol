5a2b3561a80e028cfd71a18014f95999e5359014
shaneke87 <shaneke_87@hotmail.com>
2021-12-31 13:01:01 -0600

    Fabrica de contratos

---
+++ BLOQUE 1/Fabrica/factory.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.4 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+contract SmartContract1 {
+    mapping (address => address) public MiContratoPersonal;
+
+    function Factory() public {
+        address direccion_nuevo_contrato = address (new SmartContract2(msg.sender));
+        MiContratoPersonal[msg.sender] = direccion_nuevo_contrato;
+    }
+}
+
+contract SmartContract2 {
+    address public owner;
+
+    constructor(address _direccion) public {
+        owner = _direccion;
+    }
+}
